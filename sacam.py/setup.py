91072fc6f91cb0a5e4d822846b99072804ebd862
luiz_irber <luiz_irber@ae0130e0-08c0-11de-b753-7319d013a8fa>
2006-09-15 18:09:11 +0000

    Initial commit
    git-svn-id: https://bugbrother.googlecode.com/svn@6 ae0130e0-08c0-11de-b753-7319d013a8fa

---
+++ sacam.py/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from commands import getoutput
+from distutils.core import setup, Extension
+
+cflags = getoutput('pkg-config gdk-2.0 glib-2.0 --cflags')
+temp = cflags.replace('-I', '')
+includes = temp.split()
+
+libs_output = getoutput('pkg-config gdk-2.0 glib-2.0 --libs')
+lib_dirs = []
+libs = []
+for item in libs_output.split():
+    head, tail = item[:2], item[2:]
+    if head == '-L':
+        lib_dirs.append(tail)
+    elif head == '-l':
+        libs.append(tail)
+    else:
+        print "Warning: unused flags supplied by `pkg-config gdk-2.0 glib-2.0 --libs'"
+
+videoprocessor = Extension("videoprocessor",
+                            include_dirs = includes,
+                            libraries = libs,
+#                            extra_compile_args = [''],   
+                            library_dirs = lib_dirs,
+                            sources = ['src/videoprocessormodule.c'])
+
+setup(name='SACAM',
+      version='1.0',
+      description='Sistema de Analise Comportamental de Animais em Movimento'
+                  '/ Animal Motion Behavior Analysis System ',
+      author='Luiz Carlos Irber Junior',
+      author_email='luiz.irber@gmail.com',
+      url='http://repositorio.agrolivre.gov.br/projects/sacam/',
+      ext_modules = [videoprocessor],
+      packages = ['sacam'],
+      package_dir = { 'sacam': 'src' },
+      package_data = { 'sacam': ['pixmaps/*.png'] }
+     )
+
+             
---
+++ sacam.py/src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+from interface import Interface
+
+base = Interface()
+base.main()
