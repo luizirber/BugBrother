488e6f2cff27e45159e0e75678d94ca518922db5
luiz_irber <luiz_irber@ae0130e0-08c0-11de-b753-7319d013a8fa>
2006-09-15 18:29:25 +0000

    - Some modifications on the process_video function
    git-svn-id: https://bugbrother.googlecode.com/svn@7 ae0130e0-08c0-11de-b753-7319d013a8fa

--- sacam.py/src/videoprocessor.py
+++ sacam.py/src/videoprocessor.py
@@ -1,4 +1,5 @@
 import Numeric
+from datetime import datetime, timedelta, time
 
 import pygtk
 pygtk.require('2.0')
@@ -9,26 +10,24 @@ class videoprocessor(object):
     first = None
     previous = None
     current = None
-    timer = None
     threshold = None
     
     def __init__(self):
         pass
     
-    def start_timer(self):
-        pass
-    
-    def stop_timer(self):
-        pass
-    
-    def elapsed_time(self):
-        pass
-    
     def process_video(self, source, experiment):
+        
+        if experiment.start_time == None:
+            experiment.start_time = datetime().now()
+        else:
+            begin = time().now()
+        
         pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, 1, 1)
         pixbuf = pixbuf.get_from_drawable(source.Window, 
                                    source.Window.get_colormap(),
                                    0, 0, 0, 0, -1, -1)
+        self.current = pixbuf
+        
         return True                           
         
     
